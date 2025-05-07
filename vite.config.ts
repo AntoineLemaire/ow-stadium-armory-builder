01768474bc4067cc835b8200a68c7b54cc65b62d
GGA <gabriel.gasnot@gmail.com>
2025-05-07 22:56:58 +0200

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
 
 export default defineConfig({
-  base: "/",
+  base: "/ow-stadium-armory-builder/",
   plugins: [react()],
   resolve: {
     extensions: [".js", ".ts", ".jsx", ".tsx"],
