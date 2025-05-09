8056cb5aa83162d0c0632719e04eb798307b1945
Gabriel <gabriel.gasnot@gmail.com>
2025-05-09 16:49:39 +0200

    build hero object from translation file (items remaining)

--- src/db/db.ts
+++ src/db/db.ts
@@ -18,22 +18,47 @@ const getLocalizedAttributeTypes = () => {
   );
 };
 
+/**
+ * Build the localized heroes.
+ * @returns An array of Hero.
+ */
 const getLocalizedHeroes = (): Hero[] => {
-  const t = i18n.getFixedT(null, "heroes");
-  const order = t("order", { returnObjects: true }) as number[];
+  const h = i18n.getFixedT(null, "heroes");
+  const p = i18n.getFixedT(null, "powers");
 
-  return order
-    .map<Hero | undefined>((id) => {
-      const heroData = heroes.find((h) => h.id === id);
-      if (heroData) {
-        return {
-          ...heroData,
-          id: id,
-          name: t(String(id)),
-        } as Hero;
-      }
-    })
-    .filter((h) => !!h);
+  const order = h("order", { returnObjects: true }) as number[];
+
+  // Based on "order" => get the list of hero.
+  return (
+    order
+      .map<Hero | undefined>((id) => {
+        // Get hero data from JSON
+        const heroData = heroes.find((h) => h.id === id);
+
+        // Should always be true
+        if (heroData) {
+          // Get powers from translation file
+          const powers = heroData.powers.map((powerId) =>
+            p(powerId, { returnObjects: true })
+          );
+
+          // Return Hero object.
+          return {
+            ...heroData,
+            powers: powers,
+            id: id,
+            name: h(String(id)),
+          } as Hero;
+        } else {
+          // Add warning if mismatch between order & heroes.
+          console.warn("Undefined hero detected for id: ", id);
+        }
+      })
+      // Filter out potentially "undefined" results (shouldn't happen)
+      .filter((h) => {
+        return !!h;
+      })
+  );
 };
 
 // Sort basic items
